193ab4e95d46e98f7d3db15562d587df6f792074
Almog787 <143160546+Almog787@users.noreply.github.com>
2026-04-08 15:12:42 +0300

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: '/Test2/',
-  server: {
-    // Disable HMR in production-like environments
-    hmr: process.env.DISABLE_HMR !== 'true',
-  },
-});
